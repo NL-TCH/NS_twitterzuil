d3f3b78db11481d55c13d76babd80eb991e9b6c2
TCH <teunish@outlook.com>
2021-09-10 13:22:17 +0200

    init

---
+++ Site/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/admin")
+def admin(): 
+    return render_template('login.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=8080)
