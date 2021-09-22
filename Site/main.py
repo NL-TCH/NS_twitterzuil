16bbae6318c338b0c32335640c93a91950ba002b
TCH <teunish@outlook.com>
2021-09-22 21:33:48 +0200

    loginpage no sql

--- Site/main.py
+++ Site/main.py
@@ -1,9 +1,10 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
 from static import functions
 app = Flask(__name__)
 
+
 @app.route("/", methods=["POST", "GET"])
-def home():
+def klachten():
     if request.method =='POST':
         Titel = request.form['Titel']
         Klacht = request.form['Klacht']
@@ -17,21 +18,52 @@ def home():
 
 @app.route("/login", methods=["POST","GET"])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect('/dashboard')
-    return render_template('login.html', error=error)
+    """Login Form"""
+    
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        name = request.form['username']
+        passw = request.form['password']
+        try:
+            if name == 'admin' and passw == 'admin':
+                session['logged_in'] = True
+                return redirect(url_for('dashboard'))
+            else:
+                return render_template('login.html', error='Onjuiste credentials')
+        except:
+            return render_template('login.html', error='Onjuiste credentials')
+        
+
 
 @app.route("/dashboard",methods=["POST","GET"])
 def dashboard():
-    return render_template('dashboard.html')
+    """ Session control"""
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    else:
+        if request.method == 'POST':
+            if session['logged_in']:
+                
+                return render_template('dashboard.html')
+            else:
+                return render_template('dashboard.html')
+        if session['logged_in']:
+                
+            return render_template('dashboard.html')
+        else:
+            return render_template('dashboard.html')
 
+@app.route("/logout")
+def logout():
+    """Logout Form"""
+    session['logged_in'] = False
+    return redirect(url_for('login'))
+    
 @app.route("/moderator")
 def moderator():
     return render_template('moderator.html')
 
 if __name__ == "__main__":
+    app.secret_key = "123"
     app.run(debug=True,host='0.0.0.0',port=8080)
