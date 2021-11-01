c982ea5106c9fd10212a7b26fbf5c5a11d992402
TCH <teunish@outlook.com>
2021-11-01 11:18:35 +0100

    klachten -> reviews

--- Site/static/functions.py
+++ Site/static/functions.py
@@ -9,7 +9,7 @@ access_token_secret='uBjBs8sFPyVNZLLaupNkpc1gJlWpAxgAkxZCFaPIkHD49')
     
 timelist=[datetime.datetime(2021, 1, 1, 1, 1, 1, 111111)]
 
-conn = sql.connect('host=192.168.1.52 user=NS_User password=NS_Password dbname=NS_Database')
+conn = sql.connect('host=10.27.3.100 user=NS_User password=NS_Password dbname=NS_Database')
 cursor = conn.cursor()
 
 def login(gebruikersnaam, wachtwoord):
@@ -55,79 +55,79 @@ def moderator(Naam,Titel, Klacht):
     #clean, geaccepteerd = 2
     #clean, niet geaccepteerd = 3
     if Titel_status == False or Klacht_status == False:
-        cursor.execute(f'''INSERT INTO klachten (naam,titel,klacht,scheldwoord,moderator,status) VALUES ('{Naam}','{Titel}','{Klacht}','{Scheldwoord}','1', 0);''')
+        cursor.execute(f'''INSERT INTO reviews (naam,titel,review,scheldwoord,moderator,status) VALUES ('{Naam}','{Titel}','{Klacht}','{Scheldwoord}','1', 0);''')
         conn.commit()
     elif Titel_status != False and Klacht_status != False:
-        cursor.execute(f'''INSERT INTO klachten (naam,titel,klacht,status) VALUES ('{Naam}','{Titel}','{Klacht}', 1);''')
+        cursor.execute(f'''INSERT INTO reviews (naam,titel,review,status) VALUES ('{Naam}','{Titel}','{Klacht}', 1);''')
         conn.commit()
 
 def ophalen_moderatie():
-    cursor.execute(f'''SELECT titel, klacht FROM klachten where status = '1' ORDER BY klacht_id limit 5''')
+    cursor.execute(f'''SELECT titel, review FROM reviews where status = '1' ORDER BY review_id limit 5''')
     conn.commit()
     gegevens = cursor.fetchall()
     titels = [x[0] for x in gegevens]
-    klachten = [x[1] for x in gegevens]
-    combinatie = [list(a) for a in zip(titels, klachten)]
+    reviews = [x[1] for x in gegevens]
+    combinatie = [list(a) for a in zip(titels, reviews)]
     return combinatie
 
 def ophalen_moderated():
-    #cursor.execute(f'''SELECT titel, klacht, scheldwoord, status FROM klachten where status NOT IN ('1') ORDER BY klacht_id DESC limit 5''')
+    #cursor.execute(f'''SELECT titel, review, scheldwoord, status FROM reviews where status NOT IN ('1') ORDER BY review_id DESC limit 5''')
     cursor.execute(f'''
     select 
-        klachten.titel,
-        klachten.klacht,
-        klachten.scheldwoord,
+        reviews.titel,
+        reviews.review,
+        reviews.scheldwoord,
         users.username,
-        klachten.status
-    from klachten 
+        reviews.status
+    from reviews 
     INNER JOIN users on moderator=user_id
-    where klachten.status NOT in ('1') ORDER BY klacht_id DESC limit 5
+    where reviews.status NOT in ('1') ORDER BY review_id DESC limit 5
     ''')
     conn.commit()
     gegevens =cursor.fetchall()
     titels = [x[0] for x in gegevens]
-    klachten = [x[1] for x in gegevens]
+    reviews = [x[1] for x in gegevens]
     scheldwoorden = [x[2] for x in gegevens]
     moderator = [x[3] for x in gegevens]
     status = [x[4] for x in gegevens]
-    combinatie = [list(a) for a in zip(titels,klachten,scheldwoorden,moderator,status)]
+    combinatie = [list(a) for a in zip(titels,reviews,scheldwoorden,moderator,status)]
     return combinatie
 
 def moderatie_toepassen(total,user):
     for keuze in total:
         #2.0
         if (int(keuze.replace('.',''))% 2) == 0:
-            cursor.execute(f''' UPDATE klachten
+            cursor.execute(f''' UPDATE reviews
                                 SET status = 3,
                                     moderator = (SELECT user_id from users WHERE username ='{user}')
-                                WHERE klacht_id IN (SELECT klacht_id
-                                FROM klachten
+                                WHERE review_id IN (SELECT review_id
+                                FROM reviews
                                 WHERE status =1
-                                ORDER BY klacht_id
+                                ORDER BY review_id
                                 LIMIT 1);''')
             conn.commit()
         #2.1
         elif ((int(keuze.replace('.',''))% 2) == 0) ==False:
-            cursor.execute(f''' UPDATE klachten
+            cursor.execute(f''' UPDATE reviews
                                 SET status = 2,
                                     moderator = (SELECT user_id from users WHERE username ='{user}')
-                                WHERE klacht_id IN (SELECT klacht_id
-                                FROM klachten
+                                WHERE review_id IN (SELECT review_id
+                                FROM reviews
                                 WHERE status =1
-                                ORDER BY klacht_id
+                                ORDER BY review_id
                                 LIMIT 1);''')
             conn.commit()
-            cursor.execute(f'''SELECT naam,titel,klacht
-            FROM klachten
+            cursor.execute(f'''SELECT naam,titel,review
+            FROM reviews
             WHERE status=2
-            ORDER BY klacht_id DESC
+            ORDER BY review_id DESC
 			LIMIT 1;''')
             conn.commit()
             output=cursor.fetchall()
             naam = [x[0] for x in output]
             titel = [x[1] for x in output]
-            klacht = [x[2] for x in output]
-            combinatie = [list(a) for a in zip(naam,titel,klacht)]
+            review = [x[2] for x in output]
+            combinatie = [list(a) for a in zip(naam,titel,review)]
             print(combinatie[0])
             print(combinatie[0][0])
             tweet_sturen(combinatie[0][0],combinatie[0][1],combinatie[0][2])
@@ -161,19 +161,19 @@ def filterwoorden_verwijderen(woord):
     conn.commit()
 
 def statistieken_ophalen():
-    cursor.execute(f'''select status from klachten where status = 0''')
+    cursor.execute(f'''select status from reviews where status = 0''')
     conn.commit()
     gegevens0 =cursor.fetchall()
     status0=len(gegevens0)
-    cursor.execute(f'''select status from klachten where status = 1''')
+    cursor.execute(f'''select status from reviews where status = 1''')
     conn.commit()
     gegevens1 =cursor.fetchall()
     status1=len(gegevens1)
-    cursor.execute(f'''select status from klachten where status = 2''')
+    cursor.execute(f'''select status from reviews where status = 2''')
     conn.commit()
     gegevens2 =cursor.fetchall()
     status2=len(gegevens2)
-    cursor.execute(f'''select status from klachten where status = 3''')
+    cursor.execute(f'''select status from reviews where status = 3''')
     conn.commit()
     gegevens3 =cursor.fetchall()
     status3=len(gegevens3)
@@ -182,39 +182,39 @@ def statistieken_ophalen():
 
 def gebruikers_statistieken_ophalen():
     query='''
-    SELECT users.username,klachten.moderator, COUNT(*) AS counted
-    FROM   klachten
+    SELECT users.username,reviews.moderator, COUNT(*) AS counted
+    FROM   reviews
     INNER JOIN users on moderator=user_id
     WHERE  moderator = (select mode() within group (order by moderator) as moderator
-                        from klachten
+                        from reviews
                         group by moderator
                         order by moderator DESC
                         offset 0
                         limit 1)
                         
     or moderator =		(select mode() within group (order by moderator) as moderator
-                        from klachten
+                        from reviews
                         group by moderator
                         order by moderator DESC
                         offset 1
                         limit 1)
                         
     or moderator =		(select mode() within group (order by moderator) as moderator
-                        from klachten
+                        from reviews
                         group by moderator
                         order by moderator DESC
                         offset 1
                         limit 1)
                         
     or moderator =		(select mode() within group (order by moderator) as moderator
-                        from klachten
+                        from reviews
                         group by moderator
                         order by moderator DESC
                         offset 1
                         limit 1)
                         
     or moderator =		(select mode() within group (order by moderator) as moderator
-                        from klachten
+                        from reviews
                         group by moderator
                         order by moderator DESC
                         offset 1
@@ -233,14 +233,14 @@ def gebruikers_statistieken_ophalen():
     return statistieken
     
 def tweets_ophalen():
-    cursor.execute(f'''SELECT naam, titel, klacht FROM klachten where status = '2' ORDER BY klacht_id desc limit 6''')
+    cursor.execute(f'''SELECT naam, titel, review FROM reviews where status = '2' ORDER BY review_id desc limit 6''')
     conn.commit()
 
     tweets = cursor.fetchall()
     naam = [x[0] for x in tweets]
     titel = [x[1] for x in tweets]
-    klacht = [x[2] for x in tweets]
-    tweet_list = [list(a) for a in zip(naam,titel,klacht)]
+    review = [x[2] for x in tweets]
+    tweet_list = [list(a) for a in zip(naam,titel,review)]
     return tweet_list
 
 def ophalen_gebruikers():
